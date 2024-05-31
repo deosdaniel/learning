9530199dc37adfefae6d92524826c7fde9f39b05
deosdaniel <167872952+deosdaniel@users.noreply.github.com>
2024-05-31 17:31:50 +0300

    Japan-lend

--- Practice/23.05/script.js
+++
@@ -1,8 +0,0 @@
-const navLinks = document.querySelectorAll('.navActive');
-navLinks.forEach(links => {
-    links.addEventListener('click', function(e){
-        e.preventDefault();
-        navLinks.forEach(link => link.classList.remove('active'));
-        links.classList.add('active');
-    })
-})
